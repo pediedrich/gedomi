5a52780f303d2ca8f4c717322ad39222caff875e
pablo PC STI <pablo@pc-oficina-sti.com>
2019-03-06 08:17:47 -0300

    Se modifica la columna display_name dejando como null en tabla usuarios

--- database/migrations/2019_03_06_110944_alter_column_display_name_for_users_table.php
+++ database/migrations/2019_03_06_110944_alter_column_display_name_for_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterColumnNumberForTableExpedients extends Migration
+class AlterColumnDisplayNameForUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,8 @@ class AlterColumnNumberForTableExpedients extends Migration
      */
     public function up()
     {
-        Schema::table('expedients', function (Blueprint $table) {
-            $table->string('number')->change();
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('display_name')->nullable()->change();
         });
     }
 
@@ -25,7 +25,7 @@ class AlterColumnNumberForTableExpedients extends Migration
      */
     public function down()
     {
-        Schema::table('expedients', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
         });
     }
